2a75ae75e29bed9c7d25e04b2a2045756e7463c3
latch <118853549+latchk3y@users.noreply.github.com>
2025-05-30 23:59:11 -0400

    Update run.bat

--- src/run.bat
+++ src/run.bat
@@ -3,13 +3,13 @@ setlocal
 set ZB_PATH=%APPDATA%\zig-bookmarker\zb.exe
 
 %ZB_PATH% %*
-if errorlevel 1 exit /b %errorlevel%
+if errorlevel 1 return /b %errorlevel%
 
 for /f "delims=" %%d in ('%ZB_PATH% %*') do (
     cd /d "%%d" 2>nul
     if errorlevel 1 (
         echo Failed to cd to: %%d
-        exit /b 1
+        return /b 1
     )
     echo Jumped to: %%d
 )
